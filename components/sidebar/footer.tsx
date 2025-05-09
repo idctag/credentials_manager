ff6ca86f86eb87694d3036104bcd468da1f26456
ochir <ochir.b@tanasoft.mn>
2025-05-09 11:55:18 +0800

    seperate dialog button

--- components/sidebar/footer.tsx
+++ components/sidebar/footer.tsx
@@ -18,52 +18,54 @@ import {
   useSidebar,
 } from "../ui/sidebar";
 import { signOut, useSession } from "next-auth/react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "../ui/dialog";
-import { CreateGroupForm } from "../forms/create-group";
-import useTeamStore from "@/store/team-store";
-import { useState } from "react";
+import CreateGroupButton from "./components/creat-group-button";
 
 export function NavUser() {
   const { isMobile } = useSidebar();
   const { data: session } = useSession();
-  const { activeTeam } = useTeamStore();
-  const [open, setOpen] = useState(false);
   if (!session?.user) {
     return null;
   }
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <SidebarMenu>
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenuItem>
-              <DialogTrigger asChild>
-                <SidebarMenuButton
-                  disabled={activeTeam === null}
-                  className="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground hover:cursor-pointer"
-                >
-                  <PlusCircleIcon />
-                  <span> Add group</span>
-                </SidebarMenuButton>
-              </DialogTrigger>
-            </SidebarMenuItem>
-          </SidebarGroupContent>
-        </SidebarGroup>
-        <SidebarMenuItem>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <SidebarMenuButton className="py-6 hover:cursor-pointer">
-                <Avatar>
+    <SidebarMenu>
+      <SidebarGroup>
+        <SidebarGroupContent>
+          <SidebarMenuItem>
+            <CreateGroupButton />
+          </SidebarMenuItem>
+        </SidebarGroupContent>
+      </SidebarGroup>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton className="py-6 hover:cursor-pointer">
+              <Avatar>
+                <AvatarImage
+                  src={session.user.image || "#"}
+                  alt={session.user.name || ""}
+                />
+                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">
+                  {session.user.name}
+                </span>
+                <span className="truncate text-xs">{session.user.email}</span>
+              </div>
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage
-                    src={session.user.image || "#"}
+                    src={session.user.image || ""}
                     alt={session.user.name || ""}
                   />
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
@@ -74,52 +76,19 @@ export function NavUser() {
                   </span>
                   <span className="truncate text-xs">{session.user.email}</span>
                 </div>
-              </SidebarMenuButton>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent
-              className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-              side={isMobile ? "bottom" : "right"}
-              align="end"
-              sideOffset={4}
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="hover:cursor-pointer"
+              onClick={() => signOut()}
             >
-              <DropdownMenuLabel className="p-0 font-normal">
-                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                  <Avatar className="h-8 w-8 rounded-lg">
-                    <AvatarImage
-                      src={session.user.image || ""}
-                      alt={session.user.name || ""}
-                    />
-                    <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                  </Avatar>
-                  <div className="grid flex-1 text-left text-sm leading-tight">
-                    <span className="truncate font-semibold">
-                      {session.user.name}
-                    </span>
-                    <span className="truncate text-xs">
-                      {session.user.email}
-                    </span>
-                  </div>
-                </div>
-              </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem
-                className="hover:cursor-pointer"
-                onClick={() => signOut()}
-              >
-                <LogOut />
-                Logout
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </SidebarMenuItem>
-      </SidebarMenu>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Create Group</DialogTitle>
-          <DialogDescription></DialogDescription>
-        </DialogHeader>
-        <CreateGroupForm closeDialog={() => setOpen(false)} />
-      </DialogContent>
-    </Dialog>
+              <LogOut />
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
   );
 }
